1ebf53eaf5ad971d5864c440a06178d84d9b33f3
babariviere <babariviere@protonmail.com>
2018-01-30 17:05:03 +0100

    mpsc channel instead of mutex + new colors

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,30 @@ use filler::*;
 
 use failure::Error;
 use piston_window::*;
-use std::sync::{Arc, Mutex};
+use std::sync::mpsc;
 use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
-struct Shared {
+struct Data {
     board: Vec<Board>,
+    pieces: Vec<Piece>,
     players: Vec<Player>,
     end: bool,
 }
 
-const COLOR_X: [f32; 4] = [0.8, 0.0, 0.0, 1.0];
-const COLOR_SX: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-const COLOR_O: [f32; 4] = [0.0, 0.8, 0.0, 1.0];
-const COLOR_SO: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+enum DataKind {
+    Piece(Piece),
+    Board(Board),
+    End,
+}
+
+const COLOR_X: [f32; 4] = [0.227, 0.286, 0.643, 1.0];
+const COLOR_SX: [f32; 4] = [COLOR_X[0] * 1.5, COLOR_X[1] * 1.5, COLOR_X[2] * 1.5, 1.0];
+const COLOR_O: [f32; 4] = [0.937, 0.741, 0.239, 1.0];
+const COLOR_SO: [f32; 4] = [COLOR_O[0] * 1.5, COLOR_O[1] * 1.5, COLOR_O[2] * 1.5, 1.0];
 const COLOR_NONE: [f32; 4] = [0.1, 0.1, 0.1, 1.0];
 
-const FONT: &'static [u8] = include_bytes!("../assets/font/SIMPLIFICA/SIMPLIFICA Typeface.ttf");
+const FONT: &'static [u8] = include_bytes!("../assets/font/Roboto-Regular.ttf");
 
 const FONT_SPACE: f64 = 25.0;
 
@@ -37,18 +44,17 @@ struct DisplaySettings {
     step: i8,
 }
 
-fn ui_thread(shared: Arc<Mutex<Shared>>) {
-    let size;
-    loop {
-        thread::sleep(Duration::from_millis(100));
-        let lock = shared.lock().unwrap();
-        let b = &lock.board;
-        if b.len() == 0 {
-            continue;
+fn ui_thread(mut data: Data, rx: mpsc::Receiver<DataKind>) {
+    match rx.recv().unwrap() {
+        DataKind::Board(b) => data.board.push(b),
+        _ => {
+            panic!("unexpected haha");
         }
-        size = (b[0].width() * 10, b[0].height() * 10 + 100);
-        break;
     }
+    let size = (
+        data.board[0].width() * 10,
+        data.board[0].height() * 10 + 100,
+    );
     let opengl = OpenGL::V3_2;
     let mut window: PistonWindow = WindowSettings::new("FillMe", size)
         .opengl(opengl)
@@ -64,13 +70,13 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
         instant: Instant::now(),
         board_idx: 0,
         scale: 1.,
-        speed: 50,
+        speed: 25,
         step: 1,
     };
     while let Some(e) = window.next() {
         if let Some(_r) = e.render_args() {
-            let lock = shared.lock().unwrap();
-            let b = &lock.board[ds.board_idx];
+            let loaded = data.board.len() - 1;
+            let b = &data.board[ds.board_idx];
             window.draw_2d(&e, |c, g| {
                 clear(
                     [
@@ -82,28 +88,15 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
                     g,
                 );
 
-                text_o
-                    .draw(
-                        (*lock).players[0].name(),
-                        &mut glyphs,
-                        &c.draw_state,
-                        c.transform.trans(0., FONT_SPACE),
-                        g,
-                    )
-                    .unwrap();
-
-                text_x
-                    .draw(
-                        (*lock).players[1].name(),
-                        &mut glyphs,
-                        &c.draw_state,
-                        c.transform.trans(300., FONT_SPACE),
-                        g,
-                    )
-                    .unwrap();
-
+                let mut count_p1 = 0;
+                let mut count_p2 = 0;
                 for (y, row) in b.maps().iter().enumerate() {
                     for (x, col) in row.iter().enumerate() {
+                        match *col {
+                            'o' | 'O' => count_p1 += 1,
+                            'x' | 'X' => count_p2 += 1,
+                            _ => {}
+                        }
                         let color = match *col {
                             'X' => COLOR_X,
                             'x' => COLOR_SX,
@@ -120,11 +113,48 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
                         );
                     }
                 }
+
+                text_o
+                    .draw(
+                        &format!("{}: {}", data.players[0].name(), count_p1),
+                        &mut glyphs,
+                        &c.draw_state,
+                        c.transform.trans(0., FONT_SPACE),
+                        g,
+                    )
+                    .unwrap();
+
+                text_x
+                    .draw(
+                        &format!("{}: {}", data.players[1].name(), count_p2),
+                        &mut glyphs,
+                        &c.draw_state,
+                        c.transform.trans(300., FONT_SPACE),
+                        g,
+                    )
+                    .unwrap();
+                text_x
+                    .draw(
+                        &format!("speed={}%", 100 - ds.speed),
+                        &mut glyphs,
+                        &c.draw_state,
+                        c.transform.trans(500., FONT_SPACE),
+                        g,
+                    )
+                    .unwrap();
+                text_x
+                    .draw(
+                        &format!("curr={}, loaded={}", ds.board_idx, loaded),
+                        &mut glyphs,
+                        &c.draw_state,
+                        c.transform.trans(700., FONT_SPACE),
+                        g,
+                    )
+                    .unwrap();
             });
         }
         if let Some(array) = e.resize_args() {
-            let lock = shared.lock().unwrap();
-            let b = &lock.board[ds.board_idx];
+            let b = &data.board[ds.board_idx];
             let width = (array[0] as f64) / (b.width() as f64 * 10.);
             let height = (array[1] as f64) / ((b.height() as f64 * 10.) + FONT_SPACE);
             if height > width {
@@ -144,9 +174,7 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
                 }
                 Button::Keyboard(keyboard::Key::Right) => {
                     ds.stop = true;
-                    let lock = shared.lock().unwrap();
-                    let b = &lock.board;
-                    if b.len() > (ds.board_idx + 1) {
+                    if data.board.len() > (ds.board_idx + 1) {
                         ds.board_idx += 1;
                     }
                 }
@@ -160,7 +188,9 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
                     }
                 }
                 Button::Keyboard(keyboard::Key::Minus) => {
-                    ds.speed += 5;
+                    if ds.speed <= 95 {
+                        ds.speed += 5;
+                    }
                 }
                 Button::Keyboard(keyboard::Key::N) => {
                     ds.step = -1;
@@ -175,17 +205,24 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
         }
         if !ds.stop {
             let elapsed = ds.instant.elapsed();
-            if elapsed.as_secs() >= 1 || elapsed.subsec_nanos() >= ds.speed * 1_000_000 {
-                let lock = shared.lock().unwrap();
-                let b = &lock.board;
+            if elapsed.as_secs() >= 1 || elapsed.subsec_nanos() >= ds.speed * 5_000_000 {
                 //println!("total={}, current={}", b.len(), ds.board_idx);
                 if ds.step < 0 && ds.board_idx >= (-ds.step) as usize {
                     ds.board_idx -= (-ds.step) as usize;
-                } else if b.len() > (ds.board_idx + 1) {
+                } else if data.board.len() > (ds.board_idx + 1) {
                     if ds.step > 0 {
                         ds.board_idx += ds.step as usize;
                     }
                     ds.instant = Instant::now();
+                }
+            }
+        }
+        if !data.end {
+            while let Ok(d) = rx.try_recv() {
+                match d {
+                    DataKind::Board(b) => data.board.push(b),
+                    DataKind::Piece(p) => data.pieces.push(p),
+                    DataKind::End => data.end = true,
                 }
             }
         }
@@ -194,38 +231,34 @@ fn ui_thread(shared: Arc<Mutex<Shared>>) {
 
 fn main_loop() -> Result<(), Error> {
     let players = get_players()?;
-    let shared = Arc::new(Mutex::new(Shared {
+    let data = Data {
         board: Vec::new(),
+        pieces: Vec::new(),
         players: players,
         end: false,
-    }));
-    let shared_clone = shared.clone();
+    };
+    let (tx, rx) = mpsc::channel();
     thread::spawn(move || loop {
         let buf = read_line_stdin().unwrap();
         if buf.starts_with("Plateau") {
             let board_parsed = Board::read(&buf).unwrap();
-            {
-                let mut lock = shared.lock().unwrap();
-                lock.board.push(board_parsed);
-            }
+            tx.send(DataKind::Board(board_parsed)).unwrap();
         } else if buf.starts_with("Piece") {
-            Piece::read(&buf).unwrap();
+            let piece = Piece::read(&buf).unwrap();
+            tx.send(DataKind::Piece(piece)).unwrap();
             read_line_stdin().unwrap();
         } else if buf.starts_with("==") {
             let score_o = get_score(buf);
             let score_x = get_score(read_line_stdin().unwrap());
             println!("O={}, X={}", score_o, score_x);
-            {
-                let mut lock = shared.lock().unwrap();
-                (*lock).end = true;
-            }
+            tx.send(DataKind::End).unwrap();
         } else {
             read_line_stdin().unwrap();
             break;
         }
         //thread::sleep(Duration::from_millis(5));
     });
-    ui_thread(shared_clone);
+    ui_thread(data, rx);
     Ok(())
 }
 
