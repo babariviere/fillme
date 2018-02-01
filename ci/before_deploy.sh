033c8a353be0b6e6d17c32640c6fbf4e98d6640a
babariviere <babariviere@protonmail.com>
2018-02-01 15:25:02 +0100

    try fix

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -18,7 +18,7 @@ main() {
     test -f Cargo.lock || cargo generate-lockfile
 
     # TODO Update this to build the artifacts that matter to you
-    cross rustc --bin fillme --target $TARGET --release -- -C lto
+    cross build --target $TARGET --release
 
     # TODO Update this to package the right artifacts
     cp target/$TARGET/release/fillme $CRATE_NAME-$TRAVIS_TAG-$TARGET
